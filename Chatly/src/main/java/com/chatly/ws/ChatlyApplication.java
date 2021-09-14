f34a7911c604bb2c66ba49009d45fb0381e2a489
Furkan KARAGÖZ <furkannkaragozz@gmail.com>
2021-09-14 11:18:18 +0300

    Chatly Backend section
    Initial commit for Chatly Backend section

---
+++ Chatly/src/main/java/com/chatly/ws/ChatlyApplication.java
@@ -0,0 +1,13 @@
+package com.chatly.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatlyApplication.class, args);
+    }
+
+}
