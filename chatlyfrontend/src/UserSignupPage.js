6b7ef368fbd62d25cd3049f36820cd76d308f723
Furkan KARAGÖZ <furkannkaragozz@gmail.com>
2021-09-14 14:33:53 +0300

    Update UserSignupPage.js
    Delete comments

--- chatlyfrontend/src/UserSignupPage.js
+++ chatlyfrontend/src/UserSignupPage.js
@@ -31,28 +31,3 @@ class UserSignupPage extends React.Component {
 export default UserSignupPage;
 
 
-// form avantajı mause ile değil enter ile de çalışması.
-
-
-
-// Bu oluşturduğumuz .js uzantılı sınıf node module diye de geçiyor.
-// Bu node module'ü index.js de kullanabilmemiz için aynı App.js gibi export default diye bir satır var.
-// Her node module'ünün default ya da en azından bir tane fonksiyonu,class ı export etmesi bekleniyor.
-
-// Bu yüzden "export default UserSignupPage;" yazdık class dışına. Direk classın başına export da yazabilirdik.
-// Daha sonra index.js'e geliyoruz. "import UserSignupPage from './UserSignupPage';" bu şekilde ekliyoruz oraya.
-// Daha sonra index.js de RenderDOM.render içerisindeki <App 'i <UserSignupPage/> yapıyoruz (strict mode içerisindekini)
-
-
-
- // Bu class'ın mutlaka overwrite edeceği method render methodu.
-    // Her class componentin mutlaka render methodu olması gerekiyor ve bu renderın bir jsx dönmesi gerekiyor.
-    // Burası signup page olacağı için kullanıcı bilgilerini tutmamız gerekiyor.Bu bilgileri tutmak için class 
-    // component kullanmak gerekiyor.
-    // Reactda 2 tip component bulunuyor.Biri App.js de olduğu gibi functional component.Diğeri de bu şu an yaptığımız 
-    // gibi class component.
-
-    // Diğer bir isimlendirme de App.js de ki functional componentlara stateless component deniyor.İçerisinde durum bilgisi yok.
-    // Class tipinde olanlar için de statefull deniyor.İçerisinde o componentin durumuan ilişkin bilgileri taşıyor.
-
-    // 
